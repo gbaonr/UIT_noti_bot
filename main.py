6751537a0336a872a387647dc6094aac4a16bdfc
gbaonr <gbaon.r@gmail.com>
2025-02-16 00:42:44 +0700

    fix reading env vars

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+# TOKEN = os.getenv("TOKEN")
+# CHAT_ID = os.getenv("CHAT_ID")
+TOKEN = os.environ.get("TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 URL = "https://student.uit.edu.vn/"
 DATA_FILE = "last_notification.json"
 
