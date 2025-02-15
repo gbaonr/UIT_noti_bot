4b5a0d797c37f7274d5e332e10507b287d14607f
gbaonr <gbaon.r@gmail.com>
2025-02-15 23:40:03 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,100 @@
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
+import json
+import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+URL = "https://student.uit.edu.vn/"
+DATA_FILE = "last_notification.json"
+
+
+def get_latest_notification():
+    print("[Scanning new notifications]", end="  ")
+    response = requests.get(URL)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    latest_notifications = soup.find(
+        "div", id="block-views-hien-thi-bai-viet-moi-block"
+    )
+
+    if latest_notifications:
+        notis = latest_notifications.find_all("li")
+        notis_table = []
+        for noti in notis:
+            text = noti.text.strip()
+            content = text.split("-")[0].strip()
+            date = text.split("-")[1].strip()
+            hour_minute = text.split("-")[2].strip()
+            link = noti.find("a")["href"]
+            notis_table.append(
+                {
+                    "content": content,
+                    "date": date,
+                    "hour_minute": hour_minute,
+                    "link": link,
+                }
+            )
+        return notis_table
+
+    return None
+
+
+def load_last_notification():
+    try:
+        with open(DATA_FILE, "r", encoding="utf-8") as f:
+            file = json.load(f)
+            return file
+
+    except FileNotFoundError:
+        return None
+
+
+def save_last_notification(notification):
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(notification, f, ensure_ascii=False, indent=4)
+
+
+def create_message(news):
+    text = "🔔🔔🔔🔔🔔\n"
+    for new in news:
+        text += f"<b>{new['content']}</b>\n"
+        text += f"Ngày: {new['date']} - {new['hour_minute']}\n"
+        text += f"Link: https://student.uit.edu.vn/{new['link']}\n"
+        text += "-" * 20 + "\n"
+    return text
+
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": message, "parse_mode": "HTML"}
+    response = requests.post(url, json=payload)
+    return response.json()
+
+
+def work_flow():
+    latest_notification = get_latest_notification()
+    last_notification = load_last_notification()
+
+    if not last_notification:
+        last_notification = []
+    if latest_notification and latest_notification != last_notification:
+        news = [x for x in latest_notification if x not in last_notification]
+        save_last_notification(latest_notification)
+        message = create_message(news)
+        response = send_telegram_message(message)
+        print(f"New noti(s) sent. - {datetime.now()}")
+    else:
+        print(f"No new notis found. - {datetime.now()}")
+
+
+if __name__ == "__main__":
+    while True:
+        work_flow()
+        time.sleep(5)
